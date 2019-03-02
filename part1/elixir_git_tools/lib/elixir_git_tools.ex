172367dcc8365845421a76987f8397cfe172f1b5
g4rick <285699093@qq.com>
2019-03-02 20:22:30 +0800

    init mix elixir new

---
+++ part1/elixir_git_tools/lib/elixir_git_tools.ex
@@ -0,0 +1,18 @@
+defmodule ElixirGitTools do
+  @moduledoc """
+  Documentation for ElixirGitTools.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirGitTools.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ part1/elixir_git_tools/test/elixir_git_tools_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirGitToolsTest do
+  use ExUnit.Case
+  doctest ElixirGitTools
+
+  test "greets the world" do
+    assert ElixirGitTools.hello() == :world
+  end
+end
