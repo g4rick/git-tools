8fcbf78d1760201f934e99418a18e02fcd44c411
g4rick <285699093@qq.com>
2019-03-27 17:32:19 +0800

    请求数据 但是token不对

--- part1/mr/lib/mr/gitlab_mr.ex
+++ part1/mr/lib/mr/gitlab_mr.ex
@@ -1,9 +1,21 @@
 defmodule Mr.GitlabMr do
   def fetch(host, repo) do
-    IO.inspect(mr_url(host, repo))
+    mr_url(host,repo)
+    |> HTTPoison.get!([{"PRIVATE-TOKEN", "d8e519578dfe2e28b86256d2d8f1cbeb"}])
+    |> handle_response()
   end
 
   defp mr_url(host, repo) do
     "https://#{host}/api/v4/projects/#{URI.encode_www_form(repo)}/merge_requests"
+  end
+
+  defp handle_response(%{status_code: 200, body: body}) do
+    IO.inspect(body)
+    {:ok, body}
+  end
+
+  defp handle_response(%{status_code: ___, body: body}) do
+    IO.inspect(body)
+    {:error, body}
   end
 end
